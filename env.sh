bee969f47cea6377a1f5c32731f1c3627f3bf117
Enzo <enzo.rp.90@gmail.com>
2017-08-12 12:49:57 -0500

    shhhh

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
-export AWS_ACCESS_KEY="AKIAIZT7G3QIUMAPC5ZA"
-export AWS_SECRET_KEY="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"
+export AWS_ACCESS_KEY="my_access_key"
+export AWS_SECRET_KEY="my_secret_key"
